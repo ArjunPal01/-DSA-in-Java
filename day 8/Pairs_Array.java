578d41d4396d3a22ebd97afe7014c1c18f2b9554
Arjun Pal <arjunpal1212005@gmail.com>
2025-10-15 11:53:36 +0530

    Add files via upload

---
+++ day 8/Pairs_Array.java
@@ -0,0 +1,24 @@
+public class Pairs_Array {
+    public static void printpairs(int numbers[]) {
+         int TP = 0; 
+        for(int i=0; i<numbers.length; i++) { //i=0 means 0 index wala number
+           
+            int current = numbers[i];
+            for(int j=i+1; j<numbers.length; j++) {
+                System.out.print("(" + current + "," + numbers[j] + ") ");
+                TP++;
+
+            }
+            System.out.println();
+
+        }
+        System.out.println("Total pairs = " + TP);
+
+    }
+
+    public static void main (String args[]) {
+        int numbers[] = {2, 4, 6, 8, 10};
+        printpairs(numbers);
+
+    }
+}
---
+++ day 8/Print_Subarray.java
@@ -0,0 +1,26 @@
+public class Print_Subarray {
+    public static void printSubarrays(int numbers[]) {
+        int TS = 0;
+        for(int i=0; i<numbers.length; i++) {
+            int start = i;
+            for(int j=i; j<numbers.length; j++) { //j=i+1 nhi rkha bcz hame single element wala subarray bhi print karana tha buddy
+                
+                int end = j;
+                for(int k=i; k<=end; k++) { //my mistake : k ko 0 se initiate kr rha tha
+                    System.out.print(numbers[k] + " "); //subarray
+                }
+                TS++;
+                System.out.println();
+
+            }
+            System.out.println();
+        }
+        System.out.println("Total subarrays = " + TS);
+
+    }
+    public static void main(String args[]) {
+        int numbers[] = {2, 4, 6, 8, 10};
+        printSubarrays(numbers);
+
+    }
+}

8ad09f8ee61a38e6cf41c25ed4c675a35de0fc74
Arjun Pal <arjunpal1212005@gmail.com>
2025-10-17 21:39:16 +0530

    Add files via upload

---
+++ day 9/Max_Subarray_Sum.java
@@ -0,0 +1,29 @@
+class Max_Subarray_Sum {
+    public static int MaxSubarraySum(int nums[]) {
+        
+        int maxSum = Integer.MIN_VALUE;
+        for(int i=0; i<nums.length; i++) {
+            for(int j=i; j<nums.length; j++) {
+                int currSum = 0;
+                for(int k=i; k<=j; k++) {
+                    currSum = currSum + nums[k];
+                }
+                System.out.println(currSum);
+                if(maxSum < currSum) {
+                    maxSum = currSum;
+
+                }
+
+            }
+        }
+        System.out.println("max sum : " + maxSum);
+        return maxSum;
+    }
+    
+
+    public static void main(String args[]) {
+        int nums[] = {2, 4, 6, 8, 10};
+        MaxSubarraySum(nums);
+        
+    }
+}

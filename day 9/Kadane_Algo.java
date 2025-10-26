c35f69503709e07dda44ddddf4da469f3e1b8eb0
Arjun Pal <arjunpal1212005@gmail.com>
2025-10-26 12:21:04 +0530

    Add files via upload

---
+++ day 9/Kadane_Algo.java
@@ -0,0 +1,23 @@
+class Kadane_Algo {
+    public static void KadaneAlgo(int number[]) {
+        int ms = Integer.MIN_VALUE;
+        int cs = 0;
+        for(int i=0; i<number.length; i++) {
+            cs = cs + number[i];
+
+            if (cs < 0) {
+                cs = 0;
+            }
+            
+            ms = Math.max(ms , cs);
+
+        }
+        System.out.println("Max subarray sum is : " + ms);
+        
+        
+    } 
+    public static void main (String args[]) {
+        int number[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+        KadaneAlgo(number);
+    }
+}

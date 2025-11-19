953ddadb62302a23522e035afb29548bb2d470b5
Arjun Pal <arjunpal1212005@gmail.com>
2025-11-19 16:37:21 +0530

    Add files via upload

---
+++ day 15/Recursion.java
@@ -0,0 +1,15 @@
+public class Recursion {
+    public static void printDec(int n) {
+        if(n == 1) {
+            System.out.print(n); //base case
+            return;
+        }
+        System.out.print(n + " ");
+        printDec(n-1);
+    }
+
+    public static void main(String args[]) {
+        int n = 5;
+        printDec(n);
+    }
+}
